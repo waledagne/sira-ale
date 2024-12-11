6329aea344c3df6c9dd8f851f09d58e3a3163f06
wale <walelgndagne@gmail.com>
2024-12-11 21:21:52 +0300

    fix:applying to own jobs

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -80,10 +80,12 @@
                         and attach your resume.
                     </p>
                     <div x-data="{ open: false }" id="applicant-form">
+                        @if (auth()->user()->id !== $job->user_id)
+                            <button @click = "open = true"
+                                class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium cursor-pointer text-indigo-700 bg-indigo-100 hover:bg-indigo-200">
+                                Apply Now</button>
+                        @endif
 
-                        <button @click = "open = true"
-                            class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium cursor-pointer text-indigo-700 bg-indigo-100 hover:bg-indigo-200">
-                            Apply Now</button>
                         <div x-show="open" x-cloak
                             class="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
                             <div @click.away="open = false" class="bg-white p-6 rounded-lg shadow-md w-full max-w-lg">
