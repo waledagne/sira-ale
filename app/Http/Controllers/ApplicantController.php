5c096da516b9a08167b5cd93adebe60dd31949a1
wale <walelgndagne@gmail.com>
2024-12-11 18:33:03 +0300

    comment code

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -35,7 +35,7 @@ class ApplicantController extends Controller
         $application->user_id = auth()->id();
         $application->save();
        //send email to owner
-        Mail::to($job->user->email)->send(new JobApplied($application,$job));
+        // Mail::to($job->user->email)->send(new JobApplied($application,$job));
 
         return redirect()->back()->with('success', 'Your application has been submitted!');
     }
